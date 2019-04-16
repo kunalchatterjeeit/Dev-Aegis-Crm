3ae14f086956b47b6a351596a80821d979836a82
kchattergy <kunal.chattergy@osn.com>
2019-04-16 17:52:33 +0400

    adding tab in dashboard

--- WebAppAegisCRM/Dashboard.aspx.designer.cs
+++ WebAppAegisCRM/Dashboard.aspx.designer.cs
@@ -31,24 +31,6 @@ namespace WebAppAegisCRM {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
-        /// TabContainer1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.TabContainer TabContainer1;
-        
-        /// <summary>
-        /// AccountsTab control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.TabPanel AccountsTab;
-        
-        /// <summary>
         /// DocketListDiv control.
         /// </summary>
         /// <remarks>
@@ -173,14 +155,5 @@ namespace WebAppAegisCRM {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvExpiredListAsync;
-        
-        /// <summary>
-        /// TabPanel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.TabPanel TabPanel1;
     }
 }
