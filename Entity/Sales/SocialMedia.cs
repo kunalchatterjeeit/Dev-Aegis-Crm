5d74862559df9c53137c610e44bbb449d7e68d50
Soham <kunalchatterjeeit@gmail.com>
2019-07-21 16:08:18 +0530

    social media

---
+++ Entity/Sales/SocialMedia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Sales
+{
+    public class GetSocialMediaParam
+    {
+        public int LinkTypeId { get; set; }
+        public int LinkId { get; set; }
+    }
+    public class GetSocialMedia
+    {
+        public int SocialMediaId { get; set; }
+        public string SocialMediaName { get; set; }
+        public int? SocialMediaMappingId { get; set; }
+        public int? LinkId { get; set; }
+        public int? LinkTypeId { get; set; }
+        public string UrlLink { get; set; }
+        public string Description { get; set; }
+    }
+    public class SocialMedia
+    {
+        public int Id { get; set; }
+        public int SocialMediaId { get; set; }
+        public int LinkTypeId { get; set; }
+        public int LinkId { get; set; }
+        public string URL { get; set; }
+        public string Description { get; set; }
+    }
+}
--- WebAppAegisCRM/Sales/Lead.aspx.designer.cs
+++ WebAppAegisCRM/Sales/Lead.aspx.designer.cs
@@ -283,6 +283,15 @@ namespace WebAppAegisCRM.Sales {
         protected global::System.Web.UI.WebControls.GridView gvTasks;
         
         /// <summary>
+        /// gvSocialMedia control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvSocialMedia;
+        
+        /// <summary>
         /// gvLeads control.
         /// </summary>
         /// <remarks>
