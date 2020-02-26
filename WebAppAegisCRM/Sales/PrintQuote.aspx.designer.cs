062cfcf470b192ce651e4d026cc4655164c8fe1c
Kunal <kunalchatterjeeit@gmail.com>
2020-02-26 13:32:39 +0530

    merging

--- WebAppAegisCRM/Sales/PrintQuote.aspx.designer.cs
+++ WebAppAegisCRM/Sales/PrintQuote.aspx.designer.cs
@@ -119,5 +119,23 @@ namespace WebAppAegisCRM.Sales {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblTotal;
+        
+        /// <summary>
+        /// imgSignature control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage imgSignature;
+        
+        /// <summary>
+        /// imgStump control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage imgStump;
     }
 }
